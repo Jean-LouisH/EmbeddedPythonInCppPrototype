e095cb1a31981378ff87b7ea11119a6120480bb1
Jean-LouisH <LouisLou512.e@gmail.com>
2021-02-02 19:18:34 -0500

    Tested Python.dll library calling, added CppToPython and PythonToCpp classes to main

--- src/embedded_python_in_cpp_prototype/main.cpp
+++ src/embedded_python_in_cpp_prototype/main.cpp
@@ -1,6 +1,31 @@
+#ifdef _DEBUG
+#undef _DEBUG
+#include <python.h>
+#define _DEBUG
+#else
+#include <python.h>
+#endif
 
+#include "hi_res_timer.hpp"
+#include "cpp_to_python.hpp"
+#include "python_to_cpp.hpp"
 
 int main(int argc, char* argv[])
 {
+	HiResTimer timer;
+	CppToPython cppToPython;
+	PythonToCpp pythonToCpp;
+
+	//Testing if the library is called, for now.
+	timer.setStart();
+	Py_Initialize();
+	timer.setEnd();
+	timer.printDelta_ms("Py_Initialize()");
+
+	timer.setStart();
+	PyRun_SimpleString("from time import time, ctime\nprint('Today is', ctime(time()))\n");
+	timer.setEnd();
+	timer.printDelta_ms("\nPyRun_SimpleString");
+
 	return 0;
 }
---
+++ src/embedded_python_in_cpp_prototype/python_to_cpp.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class PythonToCpp
+{
+public:
+private:
+};
